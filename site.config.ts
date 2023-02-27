825ed46477b5184df23bd88600a4b67b6aed939b
AntiEntropy-bit <AntiEntropy@pku.edu.cn>
2023-02-27 13:58:31 +0800

    Homepage

--- site.config.ts
+++ site.config.ts
@@ -54,12 +54,12 @@ export default siteConfig({
   navigationStyle: 'custom',
   navigationLinks: [
     {
-      title: 'Home',
+      title: 'Homepage',
       // pageId: 'f1199d37579b41cbabfc0b5174f4256a',
-      url: 'https://fernand.tech'
+      url: 'https://zhoushuo.tech'
     },
     {
-      title: 'Contact',
+      title: 'Email',
       // pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1',
       url: 'mailto:AntiEntropy@pku.edu.cn'
     }
