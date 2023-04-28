995249f4fb804aa01c8325028821faeb46b3dceb
AntiEntropy-bit <AntiEntropy@pku.edu.cn>
2023-04-28 17:13:35 +0800

    Enable PDF

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -12,6 +12,11 @@ import TweetEmbed from 'react-tweet-embed'
 
 // core notion renderer
 import { NotionRenderer } from 'react-notion-x'
+import { Code } from 'react-notion-x/build/third-party/code'
+import { Collection } from 'react-notion-x/build/third-party/collection'
+import { Equation } from 'react-notion-x/build/third-party/equation'
+import { Modal } from 'react-notion-x/build/third-party/modal'
+import { Pdf } from 'react-notion-x/build/third-party/pdf'
 
 // utils
 import { getBlockTitle, getPageProperty, formatDate } from 'notion-utils'
