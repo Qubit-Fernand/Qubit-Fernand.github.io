174f67c763c30deaa942d028761ac08b866d3199
AntiEntropy-bit <AntiEntropy@pku.edu.cn>
2023-10-19 19:56:33 +0800

    Rename

--- site.config.ts
+++ site.config.ts
@@ -9,7 +9,7 @@ export default siteConfig({
   rootNotionSpaceId: 'e57fa69576c34239932f087196971d29',
 
   // basic site info (required)
-  name: 'Anti-entropic Field',
+  name: "Shuo Zhou's Homepage",
   domain: 'fernand.tech',
   author: 'Fernand',
 
